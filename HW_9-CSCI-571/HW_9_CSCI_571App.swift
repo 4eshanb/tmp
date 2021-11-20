e41f9f65fc06e7677654bd7b4f31676562e2a236
4eshanb <4eshanb@gmail.com>
2021-11-20 13:49:07 -0800

    Initial Commit

---
+++ HW_9-CSCI-571/HW_9_CSCI_571App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_9_CSCI_571App.swift
+//  HW_9-CSCI-571
+//
+//  Created by Eshan Bhargava on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct HW_9_CSCI_571App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
